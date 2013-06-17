039a1ef19e645fc7d9ba01dfc6bf8182d6daa0d7
liwenke <lwkjob@163.com>
2013-06-17 16:39:47 +0800

    sts就是方便啥都自带

---
+++ src/main/java/client/TimeClientHandler.java
@@ -0,0 +1,14 @@
+package client;
+
+import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.session.IoSession;
+
+class TimeClientHandler extends IoHandlerAdapter {
+    public TimeClientHandler() {
+    }
+
+    @Override
+    public void messageReceived(IoSession session, Object message) throws Exception {
+        System.out.println(message);// 显示接收到的消息
+    }
+}
