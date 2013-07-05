e230a0c41c2f31ea3d9afc6462f5102aa68993bc
liwenke <lwkjob@163.com>
2013-07-05 09:19:44 +0800

    加注释

--- src/main/java/pattern/command/Client.java
+++ src/main/java/pattern/command/Client.java
@@ -1,13 +1,18 @@
 package pattern.command;
+
+/**
+ * @author lwk
+ * 客户点菜(client)服务员(invoker)拿着菜单(command)去找厨师(receiver)-命令模式.....
+ */
 public class Client {
 
     public static void main(String[] args) {
         //创建接收者
-        Receiver receiver = new Receiver();
+        Receiver receiver = new Receiver();//厨师
         //创建命令对象，设定它的接收者
-        Command command = new ConcreteCommand(receiver);
+        Command command = new ConcreteCommand(receiver);//菜单
         //创建请求者，把命令对象设置进去
-        Invoker invoker = new Invoker(command);
+        Invoker invoker = new Invoker(command);//服务员
         //执行方法
         invoker.action();
     }
