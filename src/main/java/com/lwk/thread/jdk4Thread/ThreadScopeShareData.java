669307995f0cf9ec44e0f3f7f1848e0682c2195a
lwkjob <lwkjob@163.com>
2014-10-19 17:24:47 +0800

    线程本地数据map实现

--- src/main/java/com/lwk/thread/jdk4Thread/ThreadScopeShareData.java
+++ src/main/java/com/lwk/thread/jdk4Thread/ThreadScopeShareData.java
@@ -7,10 +7,13 @@ import java.util.Map;
 /**
  * @author lwk
  * 用HashMap自己模拟，线程范围内的数据共享
+ * 每个线程被创建时候就给这个创建一份数据装进map里面，key就是这个线程的名称，
+ * 这个在外部取的时候用这个线程名称取出来，这样就能实现线程本地变量
  */
 public class ThreadScopeShareData {
 	private static long data = 0;
 	private static Map dataMap = new HashMap();
+	
 	public static void main(String[] args) {
 		for (int i = 0; i < 2; i++) {
 			new Thread(new Runnable() {
