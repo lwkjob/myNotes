43d290388b1279d12ca601d63048f08ff595a147
lwkjob <lwkjob@163.com>
2015-01-20 22:42:38 +0800

    mina长短连接

---
+++ src/main/java/com/lwk/mina/keepAlive/Main.java
@@ -0,0 +1,26 @@
+package com.lwk.mina.keepAlive;
+
+
+import com.lwk.mina.keepAlive.server.Initialization;
+import com.lwk.mina.keepAlive.server.MinaLongConnServer;
+import com.lwk.mina.keepAlive.server.MinaShortConnServer;
+
+/**
+ * @author lwkjob
+ *
+1）       启动服务端程序，监听8001和8002端口。
+2）       长连接客户端向服务端8002端口建立连接，服务端将连接对象保存到共享内存中。由于采用长连接方式，连接对象是唯一的。
+3）       短连接客户端向服务端8001端口建立连接。建立连接后创建一个连接对象。
+4）       短连接客户端连接成功后发送数据。服务端接收到数据后从共享内存中得到长连接方式的连接对象，使用此对象向长连接客户端发送数据。发送前将短连接对象设为长连接对象的属性值。
+5）       长连接客户端接收到数据后返回响应数据。服务端从长连接对象的属性中取得短连接对象，通过此对象将响应数据发送给短连接客户端。
+6）       短连接客户端收到响应数据后，关闭连接。
+ */
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Initialization.getInstance().init();
+		MinaShortConnServer server1 = new MinaShortConnServer();
+		server1.start();
+		MinaLongConnServer server2 = new MinaLongConnServer();
+		server2.start();
+	}
+}
---
+++ src/main/java/com/lwk/mina/keepAlive/server/MinaLongConnServerHandler.java
@@ -0,0 +1,49 @@
+package com.lwk.mina.keepAlive.server;
+
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+
+import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.core.session.IoSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MinaLongConnServerHandler extends IoHandlerAdapter {
+	private final Logger logger = (Logger) LoggerFactory.getLogger(getClass());
+
+	@Override
+	public void sessionOpened(IoSession session) {
+		InetSocketAddress remoteAddress = (InetSocketAddress) session.getRemoteAddress();
+		String clientIp = remoteAddress.getAddress().getHostAddress();
+		logger.info("Long Connect Server opened Session ID ="+String.valueOf(session.getId()));
+		logger.info("接收来自客户端 :" + clientIp + "的连接.");
+		Initialization init = Initialization.getInstance();
+		HashMap<String, IoSession> clientMap = init.getClientMap();
+		clientMap.put(clientIp, session);
+	}
+
+	@Override
+	public void messageReceived(IoSession session, Object message) {
+		logger.info("Message received in the long connect server..");
+		String expression = message.toString();
+		logger.info("Message is: " + expression);
+		IoSession shortConnSession =(IoSession) session.getAttribute("shortConnSession");
+		logger.info("Short Connect Server Session ID ="+String.valueOf(shortConnSession.getId()));
+		shortConnSession.write(expression);
+	}
+
+	@Override
+	public void sessionIdle(IoSession session, IdleStatus status) {
+		logger.info("Disconnecting the idle.");
+		// disconnect an idle client
+		session.close(true);
+	}
+
+	@Override
+	public void exceptionCaught(IoSession session, Throwable cause) {
+		// close the connection on exceptional situation
+		logger.warn(cause.getMessage(), cause);
+		session.close(true);
+	}
+}
