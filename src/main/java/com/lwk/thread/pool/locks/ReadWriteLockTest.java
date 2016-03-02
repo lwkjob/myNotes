bd82ad719d524d765b451c7ea9664afaed30d1f1
李文科 <wenke.li@dianrong.com>
2016-03-02 19:44:13 +0800

    读写锁设置为公平锁

--- src/main/java/com/lwk/thread/pool/locks/ReadWriteLockTest.java
+++ src/main/java/com/lwk/thread/pool/locks/ReadWriteLockTest.java
@@ -1,8 +1,16 @@
 package com.lwk.thread.pool.locks;
 
 
+import com.google.common.base.Joiner;
+
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
@@ -14,27 +22,28 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 public class ReadWriteLockTest {
 	
 	public static void main(String[] args) {
-		
+
 		final Queue3 q3 = new Queue3();
 		for(int i=0;i<3;i++)
 		{
-			new Thread(){
-				public void run(){
-					while(true){
-						q3.get();						
-					}
-				}
-				
-			}.start();
 
-			new Thread(){
-				public void run(){
-					while(true){
-						q3.put(new Date().getTime());
-					}
-				}			
-				
-			}.start();
+			new Thread(()-> {
+				while(true){
+					q3.get();
+
+				}
+			},"lwk_____read"+i).start();
+
+			new Thread(()-> {
+				while(true){
+					q3.put(new Date().getTime());
+
+				}
+			},"lwk_____write"+i).start();
+
+
+
+
 		}
 		
 	}
@@ -42,14 +51,16 @@ public class ReadWriteLockTest {
 
 class Queue3{
 	//共享数据，只能有一个线程能写该数据，但可以有多个线程同时读该数据。
-	private Object data = null;
-	ReadWriteLock rwl = new ReentrantReadWriteLock();
+	private  List<String> data = new CopyOnWriteArrayList<>();
+
+	ReadWriteLock rwl = new ReentrantReadWriteLock(true);//设置为公平锁
+
 	public void get(){
-		rwl.readLock().lock();
 		try {
+			rwl.readLock().lock();
 			System.out.println(Thread.currentThread().getName() + " 准备读");
-			Thread.sleep(100);
-			System.out.println(Thread.currentThread().getName() + "读完了" + data);			
+			Thread.sleep(1000);
+			System.out.println(Thread.currentThread().getName() + "读完了" + Joiner.on(",").join(this.data));
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}finally{
@@ -59,12 +70,12 @@ class Queue3{
 	
 	public void put(Object data){
 
-		rwl.writeLock().lock();
 		try {
-//			System.out.println(Thread.currentThread().getName() + " 准备写");					
-			Thread.sleep(10000);
-			this.data = data;		
-//			System.out.println(Thread.currentThread().getName() + " 写完了 " + data);					
+			rwl.writeLock().lock();
+			System.out.println(Thread.currentThread().getName() + " 准备写");
+			Thread.sleep(1000);
+			this.data.add(data+"");
+			System.out.println(Thread.currentThread().getName() + " 写完了 ");
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}finally{
